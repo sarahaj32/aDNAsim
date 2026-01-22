f29027a9c4efdb7f0421e624c312dd54bc910542
sarahaj32 <sarahj32@berkeley.edu>
2026-01-22 13:31:25 -0800

    added option to deaminate a proportion of sites

--- src/deaminate_sim.py
+++ src/deaminate_sim.py
@@ -3,7 +3,7 @@ Helper file with functions to deaminate genotypes of a
 target individual. 
 """
 
-from helper_functions import parse_header
+from helper_functions import parse_header, multiallelic
 import random
 
 # function to create the heterozygous deaminated genome call
@@ -29,11 +29,12 @@ def deam_geno_call(base, rate):
     else:
         return(f"0{base[1]}1{base[3:]}")
 
-def add_deam(vcf_path, new_vcf, sample_list, rate):
+def add_deam(vcf_path, new_vcf, sample_list, rate, ts_proportion):
     """
     converts homozygous reference transition genotypes 
     to heterozygous at specified deamination rate. 
-    homozgyous alternative positions become heterozygous
+    Transtions are either defined by the alleles in the VCF,
+    or specified as a proportion of all sites (ts_proportion).
     """
     with open(vcf_path, "r") as file1, open(new_vcf, "w") as outfile:
         for line in file1:
@@ -41,17 +42,37 @@ def add_deam(vcf_path, new_vcf, sample_list, rate):
             if line[0].startswith("#"): 
                 outfile.write("\t".join(line) + "\n")
                 if "#CHROM" in line[0]:
-                    header_ix, include = parse_header(line, sample_list)
-                    print(f"deaminating {sample_list}")
-                    print(f"include:{include}")
+                    # header_idx: dictionary with indices of VCF header columns
+                    # include: the indices of the samples for the simulation to be applied to
+                    # names: the sample names corresponding to the include indices
+                    header_ix, include, names = parse_header(line, sample_list)
+                    
+                    # print information about the samples being deaminated
+                    if sample_list != []:
+                        print(f"\nmaking {names} samples deaminated at {rate} rate\n")
+                    else:
+                        print(f"\nmaking all samples deaminated at {rate} rate\n")
+                    
+                    # print information about the deamination simulation
+                    if ts_proportion:
+                        print(f"\ndefining transition sites as a {ts_proportion} of all sites\n")
+                    else:
+                        print(f"\ndefining transition sites based on VCF alleles\n")
             else:
-                # find the transition sites and add deamination
-                # at all transition sites, convert homozygous reference to heterozygous 
-                # what we should actually do otherwise is set a proportion of positions as transitions (ie: 77.6) and ignore the alleles themselves
-                if (line[header_ix["ref_ix"]] == "C" and line[header_ix["alt_ix"]] == "T") or (line[header_ix["ref_ix"]] == "G" and line[header_ix["alt_ix"]] == "A"):
-                    line = [deam_geno_call(line[i], rate) if i in include else line[i] for i in range(len(line))]
-                # could remove this line to more closely mimic what happens biologically (probably not necessary if we're just trying to induce biased error)
-                elif (line[header_ix["ref_ix"]] == "T" and line[header_ix["alt_ix"]] == "C") or (line[header_ix["ref_ix"]] == "A" and line[header_ix["alt_ix"]] == "G"):
-                    line = [deam_geno_call(line[i], rate) if i in include else line[i] for i in range(len(line))]
-                # the line may be updated by the above 2 steps or not. Regardless, write it out
-                outfile.write("\t".join(line) + "\n")
+                if not multiallelic(line, header_ix):
+                    # if we are defining transition sites as a proportion of all sites:
+                    if ts_proportion:
+                        # determine if this is a transition site based on that proportion
+                        if random.random() <= ts_proportion:
+                            line = [deam_geno_call(line[i], rate) if i in include else line[i] for i in range(len(line))]
+                    
+                    # otherwise, use the alleles in the VCF to determine if it's a transition site
+                    else:
+                        # find the transition sites and add deamination
+                        # at all transition sites, convert homozygous reference to heterozygous 
+                        if (line[header_ix["ref_ix"]] == "C" and line[header_ix["alt_ix"]] == "T") or (line[header_ix["ref_ix"]] == "G" and line[header_ix["alt_ix"]] == "A"):
+                            line = [deam_geno_call(line[i], rate) if i in include else line[i] for i in range(len(line))]
+                        elif (line[header_ix["ref_ix"]] == "T" and line[header_ix["alt_ix"]] == "C") or (line[header_ix["ref_ix"]] == "A" and line[header_ix["alt_ix"]] == "G"):
+                            line = [deam_geno_call(line[i], rate) if i in include else line[i] for i in range(len(line))]
+                    # the line may be updated by the above steps or not. Regardless, write it out
+                    outfile.write("\t".join(line) + "\n")
