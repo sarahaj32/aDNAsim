41cfe5a6e425082745df34c91bc837ae8d5ae5e7
sarahaj32 <sarahj32@berkeley.edu>
2026-01-22 11:23:46 -0800

    added option to input json of samples, improved flexibility and notifcations

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,3 +1,26 @@
+import json
+
+def parse_indivs(samp_input, ind_type):
+    # create a list of individuals from either a json file or a comma-separated string
+    if ".json" in samp_input:
+        return(parse_indivs_from_json(samp_input, ind_type))
+    sample_list = samp_input.strip()
+    if sample_list == "":
+        sample_list = []
+    else:
+        sample_list = [s.strip() for s in sample_list.split(",")]  
+    return(sample_list)
+
+def parse_indivs_from_json(samp_json, ind_type):
+    with open(samp_json, "r") as f:
+        data = json.load(f)
+    # make all keys lowercase in case they were typed differently in the json file
+    data = {key.lower():value for key,value in data.items()}
+    # select the target or contam individuals
+    samples = data[ind_type]
+    sample_list = [s.strip() for s in samples]  
+    return(sample_list)
+
 def parse_header(line, sample_list):
     """
     A helper function that parses the header line of a VCF file
@@ -15,22 +38,16 @@ def parse_header(line, sample_list):
     header_ix = {"ref_ix":ref_ix, "alt_ix":alt_ix, "pos_ix":pos_ix, "format_ix":format_ix} 
     
     if sample_list == []:
-        # use all samples (so just remove VCF specific rows)
+        # if there are no specified samples, use all samples (so just remove VCF specific rows)
         exclude = ["#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO", "FORMAT"]
-        include = [i for i,name in enumerate(line) if name not in exclude]
-        sample_list = [name for i,name in enumerate(line) if name not in exclude]
+        include_idx = [i for i,name in enumerate(line) if name not in exclude]
     else:
         # get index of specified samples
-        include = [i for i,name in enumerate(line) if any([f"{s}_" in name for s in sample_list])]
-    if include == []:
-        # print("provided population names not in file. Breaking")
-        raise Exception("provided population names not in file. Breaking")
-    return header_ix, include
-
-def parse_target_indivs(input):
-    sample_list = input.strip()
-    if sample_list == "":
-        sample_list = []
-    else:
-        sample_list = [s.strip() for s in sample_list.split(",")]  
-    return(sample_list)
+        include_idx = [i for i,name in enumerate(line) if any([f"{s}" in name for s in sample_list])]
+        missing = [s for s in sample_list if not any([s in name for name in line])]
+        if missing != []:
+            print(f"\nWarning: the following specified samples were not found in the VCF and will be ignored: {missing}\n")
+    if include_idx == []:
+        raise Exception("None of the provided sample names were found in file. Breaking")
+    include_names = [line[i] for i in include_idx]
+    return header_ix, include_idx, include_names
